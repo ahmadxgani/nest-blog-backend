154cbb825cff72c1ce6182a4cc5857ce99b8b6b5
QUERY-GANI <ahmadxgani01@gmail.com>
2021-07-17 18:48:01 +0700

    confused unga bunga

--- src/jwt/strategy.jwt.ts
+++ src/jwt/strategy.jwt.ts
@@ -1,8 +1,10 @@
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AuthService } from 'src/modules/author/auth/auth.service';
 import { AuthorDocument } from 'src/modules/author/author.model';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly authService: AuthService) {
     super({
